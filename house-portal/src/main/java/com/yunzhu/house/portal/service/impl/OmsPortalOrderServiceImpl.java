2d04096ec0119b69846f9b90d42a98cf931327ba
shw <shw20220801@163.com>
2022-08-06 11:40:47 +0800

    fix it

---
+++ house-portal/src/main/java/com/yunzhu/house/portal/service/impl/OmsPortalOrderServiceImpl.java
@@ -0,0 +1,14 @@
+package com.yunzhu.house.portal.service.impl;
+
+import com.yunzhu.house.portal.service.OmsPortalOrderService;
+import org.springframework.stereotype.Service;
+
+/**
+ * TODO
+ *
+ * @author DELL
+ * @date 2022/8/6 11:30
+ */
+@Service
+public class OmsPortalOrderServiceImpl implements OmsPortalOrderService {
+}
